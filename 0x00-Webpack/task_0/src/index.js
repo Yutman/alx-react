00909f9ff44e9284aab722ce185af870a81da713
Yutman <kuriapeter95@gmail.com>
2023-10-26 17:53:10 +0300

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,23 @@
+// Import jQuery
+import $ from "jquery";
+
+// Function to add paragraphs to the page body
+function addParagraphs() {
+  const body = $("body");
+
+  // Create and append the first paragraph
+  const paragraph1 = $("<p>Holberton Dashboard</p>");
+  body.append(paragraph1);
+
+  // Create and append the second paragraph
+  const paragraph2 = $("<p>Dashboard data for the students</p>");
+  body.append(paragraph2);
+
+  // Create and append the third paragraph
+  const paragraph3 = $("<p>Copyright - Holberton School</p>");
+  body.append(paragraph3);
+}
+// Call the addParagraphs function when the document is ready
+$(document).ready(function () {
+  addParagraphs();
+});
