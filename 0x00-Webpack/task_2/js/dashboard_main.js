1bc4532bd903c99c8c11168b488769918643f442
Yutman <kuriapeter95@gmail.com>
2023-10-26 21:49:02 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,45 +1,24 @@
-// Import jQuery and load dash
+// interesting jquery import thanks to babel
 import $ from "jquery";
-
-// Create and append the elements
-$(document).ready(function () {
-  // Create and append the first paragraph
-  const paragraph1 = $("<p>Holberton Dashboard</p>");
-  $("body").append(paragraph1);
-
-  // Create and append the second paragraph
-  const paragraph2 = $("<p>Dashboard data for the students</p>");
-  $("body").append(paragraph2);
-
-  // Create and append the button element
-  const button = $("<button>Click here to get started</button>");
-  $("body").append(button);
-
-  // Create and append the paragraph with id='count'
-  const countParagraph = $('<p id="count"></p>');
-  $("body").append(countParagraph);
-
-  // Create and append the last paragraph
-  const paragraph3 = $("<p>Copyright - Holberton School</p>");
-  $("body").append(paragraph3);
-});
-
-import $ from "jquery";
+// import lodash
 import _ from "lodash";
+// styles
+import "../css/main.css";
 
-// Initialize a click counter
-let clickCount = 0;
+// append elements
+$("body").append(`<div id="logo"></div>`);
+$("body").append(`<p>Holberton Dashboard</p>`);
+$("body").append(`<p>Dashboard data for the students</p>`);
+$("body").append(`<button>Click here to get started</button>`);
+$("body").append(`<p id="count"></p>`);
+$("body").append(`<p>Copyright - Holberton School</p>`);
 
-// Function to update the click count and display it in the paragraph
+// bind lodash's debounce to the button
+$("button").on("click", _.debounce(updateCounter, 500));
+
+let count = 0;
+// function to update the counter
 function updateCounter() {
-  clickCount += 1;
-  const countParagraph = $("#count");
-  countParagraph.text(
-    `${clickCount} click${clickCount !== 1 ? "s" : ""} on the button`
-  );
+  count++;
+  $("#count").text(`${count} clicks on the button`);
 }
-// Bind the debounced updateCounter function to the button click event
-const debouncedUpdateCounter = _.debounce(updateCounter, 1000); // Adjust the debounce delay (in milliseconds) as needed
-
-// When the button is clicked, execute the debounced function
-$("#clickButton").on("click", debouncedUpdateCounter);
