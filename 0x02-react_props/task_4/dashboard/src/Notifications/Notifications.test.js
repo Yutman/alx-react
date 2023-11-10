6b19ce74d772e8c13a9baa467865e4e58c879660
Yutman <kuriapeter95@gmail.com>
2023-11-10 18:14:11 +0300

    Props types & Default props & Shapes

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -2,16 +2,42 @@ import React from "react";
 import { shallow } from "enzyme";
 import Notifications from "./Notifications";
 
-const wrapper = shallow(<Notifications />);
+describe("<Notification />", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<Notifications />);
+    shallow(<Notifications />);
+  });
 
-it("renders without crashing", () => {
-  shallow(<Notifications />);
-});
-it("renders three list items", () => {
-  expect(wrapper.find("li").children().length).toEqual(3);
-});
-it("renders the <p>", () => {
-  expect(
-    wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
-  ).toBeTruthy();
+  it("Notification Item with html", () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    const nItem = wrapper.find("NotificationItem");
+    expect(nItem).toBeDefined();
+    expect(nItem.first().html()).toEqual(
+      '<li data-notification-type="default">New course available</li>'
+    );
+  });
+
+  it("menuItem with displayDrawer false", () => {
+    const wrapper = shallow(<Notifications />);
+    const mItem = wrapper.find("div.menuItem");
+    expect(mItem).toHaveLength(1);
+  });
+
+  it("Notification with displayDrawer false", () => {
+    const wrapper = shallow(<Notifications />);
+    const dNoti = wrapper.find("div.Notifications");
+    expect(dNoti).toHaveLength(0);
+  });
+
+  it("menuItem with displayDrawer true", () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    const mItem = wrapper.find("div.menuItem");
+    expect(mItem).toHaveLength(1);
+  });
+
+  it("displayDrawer is true", () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    const dNoti = wrapper.find("div.Notifications");
+    expect(dNoti).toHaveLength(1);
+  });
 });
