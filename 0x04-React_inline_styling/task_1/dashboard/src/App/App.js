32ce77f74f4f49d32e340eaad20f63131346700a
Yutman <kuriapeter95@gmail.com>
2023-11-15 21:14:12 +0300

    install Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,88 +1,91 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import Header from "../Header/Header";
-import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
-import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
-import BodySection from "../BodySection/BodySection";
+import Notifications from "../Notifications/Notifications";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import "./App.css";
 import PropTypes from "prop-types";
 import { getLatestNotification } from "../utils/utils";
-import { StyleSheet, css } from "aphrodite";
+import BodySection from "../BodySection/BodySection";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleLogout = this.handleLogout.bind(this);
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleLogout);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleLogout);
-  }
-  handleLogout(e) {
+
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  handleKeyPress(e) {
     if (e.ctrlKey && e.key === "h") {
-      e.preventDefault();
       alert("Logging you out");
       this.props.logOut();
     }
   }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
   render() {
-    const listCourses = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
-    const listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-    ];
-    const { isLoggedIn } = this.props;
     return (
-      <Fragment>
-        <Notifications listNotifications={listNotifications} />
-        <Header />
-        {isLoggedIn ? (
-          <BodySectionWithMarginBottom title="Course list">
-            <CourseList listCourses={listCourses} />
-          </BodySectionWithMarginBottom>
-        ) : (
-          <BodySectionWithMarginBottom title="Log in to continue">
-            <Login />
-          </BodySectionWithMarginBottom>
-        )}
-        <BodySection title="News from the School">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </BodySection>
-        <div className={css(styles.footer)}>
+      <React.Fragment>
+        <div className="App">
+          <div className="heading-section">
+            <Notifications listNotifications={this.listNotifications} />
+            <Header />
+          </div>
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the school">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis at tempora odio, necessitatibus repudiandae
+              reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
+              ipsa iste vero dolor voluptates.
+            </p>
+          </BodySection>
           <Footer />
         </div>
-      </Fragment>
+      </React.Fragment>
     );
   }
 }
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => undefined,
+  logOut: () => {
+    return;
+  },
 };
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
 };
-
-const styles = StyleSheet.create({
-  footer: {
-    width: "100%",
-    position: "fixed",
-    bottom: 0,
-    textAlign: "center",
-    fontStyle: "italic",
-    borderTop: "thick solid #e0344a",
-  },
-});
 
 export default App;
