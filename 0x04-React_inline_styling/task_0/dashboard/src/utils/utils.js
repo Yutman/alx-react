5b1df140eb52ef34072bdbf94e4a4825b3228d12
Yutman <kuriapeter95@gmail.com>
2023-11-17 12:16:31 +0300

    Inline styling

--- 0x04-React_inline_styling/task_0/dashboard/src/utils/utils.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/utils/utils.js
@@ -1,7 +1,7 @@
 export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
-  isIndex ? "Holberton School" : "Holberton School main dashboard";
+	isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 
 export const getLatestNotification = () =>
-  "<strong>Urgent Requirement</strong> - complete by EOD";
+	'<strong>Urgent Requirement</strong> - complete by EOD';
