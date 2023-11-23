4b5e49463dbe45f4f4275e10bc9db4829c4e54dc
Yutman <kuriapeter95@gmail.com>
2023-11-23 15:30:06 +0300

    Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -4,6 +4,7 @@ import {
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
 } from "./uiActionTypes";
+import fetch from "node-fetch";
 
 export function login(email, password) {
   return {
@@ -31,3 +32,26 @@ export const hideNotificationDrawer = () => ({
 
 export const boundHideNotificationDrawer = () =>
   dispatch(hideNotificationDrawer());
+
+export function loginSuccess() {
+  return {
+    type: LOGIN_SUCCESS,
+  };
+}
+
+export function loginFailure() {
+  return {
+    type: LOGIN_FAILURE,
+  };
+}
+
+export function loginRequest(email, password) {
+  return (dispatch) => {
+    boundLogin(email, password);
+
+    return fetch("http://localhost:8564/login-success.json")
+      .then((res) => res.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((error) => dispatch(loginFailure()));
+  };
+}
